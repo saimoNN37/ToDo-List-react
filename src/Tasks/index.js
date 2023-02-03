24eceac4ef549d714acc38d35d79e965aba956a6
Szymon Mlynski <szymon.mlynski03@gmail.com>
2023-02-03 12:22:48 +0100

    task list without interactions yet

---
+++ src/Tasks/index.js
@@ -0,0 +1,23 @@
+import "./style.css";
+
+const Tasks = (props) => (
+    <ul className="tasks">
+        {props.tasks.map(task => (
+            <li
+                className={`tasks__list${task.done && props.hideDoneTasks ? "tasks__list--hidden" : ""}`}
+            >
+                <button className="tasks__button tasks__button--done">
+                    {task.done ? "✔" : ""}
+                </button>
+                <span className={`tasks__taskName${task.done ? " tasks__taskName--done" : ""}`}>
+                    {task.content}
+                </span>
+                <button className="tasks__button">
+                    🗑
+                </button>
+            </li>
+        ))}
+    </ul>
+);
+
+export default Tasks;
