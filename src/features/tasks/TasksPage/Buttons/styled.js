3f804a2909d47f7c9a679f2b77d97af2794c77ae
Szymon Mlynski <szymon.mlynski03@gmail.com>
2023-03-10 13:25:42 +0100

    small changes

--- src/features/tasks/TasksPage/Buttons/styled.js
+++ src/features/tasks/TasksPage/Buttons/styled.js
@@ -5,25 +5,3 @@ export const ButtonList = styled.div`
     flex-wrap: wrap;
 `;
 
-export const StyledButtons = styled.button`
-    border: none;
-    margin: 0 0 0 20px;
-    color: hsl(180, 100%, 25%);
-    background-color: transparent;
-    transition: 1s;
-    cursor: pointer;
-
-    &:hover {
-        color: hsl(180, 100%, 35%);
-    }
-
-    &:disabled {
-        color: #ccc;
-    }
-
-    @media (max-width: 767px) {
-        flex-basis: 100%;
-        margin: 10px;
-    }
-
-`;
