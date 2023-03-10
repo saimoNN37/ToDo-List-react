72fc5a172481c17de7d83ac45bb8b4513cb4bb69
Szymon Mlynski <szymon.mlynski03@gmail.com>
2023-03-10 08:44:04 +0100

    persist tasks in localStorage(again)

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,29 +1,35 @@
 import { ButtonList, StyledButtons } from "./styled";
 import { useSelector, useDispatch } from "react-redux";
-import {  toogleHideDone, setAllDone, selecHideDone, selectAreTasksEmpty, selectIsEveryTaskDone } from "../taskSlice";
+import { toogleHideDone, setAllDone, selecHideDone, selectAreTasksEmpty, selectIsEveryTaskDone, fetchExampleTasks } from "../taskSlice";
 
 
 const Buttons = () => {
-    const hideDone  = useSelector(selecHideDone);
+    const hideDone = useSelector(selecHideDone);
     const isEmptyTasks = useSelector(selectAreTasksEmpty);
     const allTaskDone = useSelector(selectIsEveryTaskDone);
+
     const disPatch = useDispatch();
     return (
-        !isEmptyTasks && (
-            <ButtonList>
-                <StyledButtons
-                    onClick={() => disPatch(toogleHideDone())}
-                >
-                    {hideDone ? "Pokaż" : "Ukryj"} ukończone
-                </StyledButtons>
-                <StyledButtons
-                    disabled={allTaskDone}
-                    onClick={() => disPatch(setAllDone())}
-                >
-                    Ukończ wszystkie
-                </StyledButtons>
-            </ButtonList>
-        )
+        <ButtonList>
+            <StyledButtons onClick={() => disPatch(fetchExampleTasks())}>
+                Pobierz przykładowe zadania
+            </StyledButtons>
+            {!isEmptyTasks && (
+                <>
+                    <StyledButtons
+                        onClick={() => disPatch(toogleHideDone())}
+                    >
+                        {hideDone ? "Pokaż" : "Ukryj"} ukończone
+                    </StyledButtons>
+                    <StyledButtons
+                        disabled={allTaskDone}
+                        onClick={() => disPatch(setAllDone())}
+                    >
+                        Ukończ wszystkie
+                    </StyledButtons>
+                </>
+            )}
+        </ButtonList>
     )
 };
 
---
+++ src/features/tasks/tasksSaga.js
@@ -0,0 +1,23 @@
+import { takeEvery, call, put, select } from "redux-saga/effects";
+import { getExampleTasks } from "./getExampleTasks";
+import { fetchExampleTasks, selecTasks, setTasks } from "./taskSlice";
+import { saveTasksInLocalStorage } from "./tasksLocalStorage";
+
+function* fetchExampleTasksHandler() {
+    try {
+        const exampleTasks = yield call(getExampleTasks);
+        yield put(setTasks(exampleTasks));
+    } catch (error) {
+        yield call(alert, "Coś poszło nie tak!");
+    }
+}
+
+function* saveTasksInLocalStorageHandler() {
+    const tasks = yield select(selecTasks);
+    yield call(saveTasksInLocalStorage, tasks);
+}
+
+export function* tasksSaga() {
+    yield takeEvery(fetchExampleTasks.type, fetchExampleTasksHandler);
+    yield takeEvery("*", saveTasksInLocalStorageHandler)
+}
