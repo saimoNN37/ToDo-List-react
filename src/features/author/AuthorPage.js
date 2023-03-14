f456fc8ce4f7e71126c9953b871268f040571cae
Szymon Mlynski <szymon.mlynski03@gmail.com>
2023-03-14 14:57:23 +0100

    added text about author

--- src/features/author/AuthorPage.js
+++ src/features/author/AuthorPage.js
@@ -3,12 +3,18 @@ import Header from "../../common/Header";
 import Section from "../../common/Section";
 
 export default () => (
-<Container>
-      <Header title="O autorze" />
-      <Section
-        title="Szymon Młyński"
-        body={<>sddsds</>}
-      />
-      </Container>
+  <Container>
+    <Header title="O autorze" />
+    <Section
+      title="Szymon Młyński"
+      body={<>
+        <p>Cześć mam na imię Szymon Młyński mam 19 lat i zaczynam swoją przygodę z programowaniem.
+          Ukończyłem szkołę średnią w <strong>Miechowie</strong> jest to miasto niedaleko Krakowa.</p>
+        <p>Moim głównym zajęciem jest chodzenie na siłownię, która pozwala mi się zrelaksować po całym dniu.
+          W ciągu dnia uwielbiam obejrzeć odcinki anime.
+          Pozwala mi się chociaż przez chwilę odciać od rzeczywistości.</p>
+      </>}
+    />
+  </Container>
 
 );
