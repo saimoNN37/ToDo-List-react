7d67b67388fb1dfce459ccc11889af539d88c5b9
Szymon Mlynski <szymon.mlynski03@gmail.com>
2023-03-14 14:49:18 +0100

    added hooks

--- src/features/tasks/TasksPage/Search/index.js
+++ src/features/tasks/TasksPage/Search/index.js
@@ -1,23 +1,18 @@
 import { useHistory, useLocation } from "react-router-dom";
 import Input from "../../Input"
+import { useQueryParameters, useReplaceQueryParameters } from "../queryParameters";
 import searchQueryParamName from "../searchQueryParamName";
 import { Wrapper } from "./styled";
 
 export default () => {
-    const location = useLocation();
-    const history = useHistory();
-    const query = new URLSearchParams(location.search).get(searchQueryParamName);
+    const query = useQueryParameters(searchQueryParamName);
+    const replaceQueryParameter = useReplaceQueryParameters();
 
     const onInputChange = ({target}) => {
-        const searchParams = new URLSearchParams(location.search);
-
-        if(target.value.trim() === "") {
-            searchParams.delete(searchQueryParamName);
-        } else {
-            searchParams.set(searchQueryParamName, target.value)
-        }
-
-        history.push(`${location.pathname}?${searchParams.toString()}`)
+        replaceQueryParameter({
+            key: searchQueryParamName,
+            value: target.value.trim() !== "" ? target.value : undefined,
+        });
     };
 
     return (
